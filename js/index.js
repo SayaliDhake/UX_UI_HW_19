ec065eb25ab08d2963021f681e93faf913393fed
SayaliDhake <61599886+SayaliDhake@users.noreply.github.com>
2020-08-29 15:57:57 -0400

    Updated Portfolio pages
    home page is updated

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// toggle effect to navigation bar//
+  $(document).ready(function(){
+    $(".arrow").click(function(){
+      $(".home").fadeToggle();
+    });
+
+    $(".skillPage").click(function(){
+        window.location.href = "myskills.html"
+    })
+
+    $(".aboutPage").click(function(){
+        window.location.href = "index.html"
+    })
+
+    $(".workPage").click(function(){
+        window.location.href = "mywork.html"
+    })
+  });
+
+//arrow animation/
+
